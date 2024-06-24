ff982c47185aab7abb481a44761625d5c7e8b63f
pandaantonio <pedro.antoniominecraft01@gmail.com>
2024-06-23 21:22:14 -0300

    ok

--- lib/index.ts
+++ lib/index.ts
@@ -1,42 +1,57 @@
-import ChatInputCommandBuilder from "./ApplicationCommand/ChatInputCommandBuilder";
-import MessageCommandBuilder from "./ApplicationCommand/MessageCommandBuilder";
-import AttachmentOptionBuilder from "./ApplicationCommand/options/AttachmenteOptionBuilder";
-import BooleanOptionBuilder from "./ApplicationCommand/options/BooleanOptionBuilder";
-import ChannelOptionBuilder from "./ApplicationCommand/options/ChannelOptionBuilder";
-import IntegerOptionBuilder from "./ApplicationCommand/options/IntegerOptionBuilder";
-import MentionableOptionBuilder from "./ApplicationCommand/options/MentionableOptionBuilder";
-import NumberOptionBuilder from "./ApplicationCommand/options/NumberOptionBuilder";
-import RoleOptionBuilder from "./ApplicationCommand/options/RoleOptionBuilder";
-import StringOptionBuilder from "./ApplicationCommand/options/StringOptionBuilder";
-import SubCommandGroupOptionBuilder from "./ApplicationCommand/options/SubCommandGroupOptionBuilder";
-import SubCommandOptionBuilder from "./ApplicationCommand/options/SubCommandOptionBuilder";
-import UserOptionBuilder from "./ApplicationCommand/options/UserOptionBuilder";
-import UserCommandBuilder from "./ApplicationCommand/UserCommandBuilder";
-import CanvasUtil from "./Canvas";
-import EmbedBuilder from "./Message/EmbedBuilder";
-import EmbedColors from "./Message/EmbedColors";
-import getEmoji from "./Utilities/getEmoji";
-import GifAPI from "./Utilities/GifAPI";
+import _ChatInputCommandBuilder from "./ApplicationCommand/ChatInputCommandBuilder";
+import _MessageCommandBuilder from "./ApplicationCommand/MessageCommandBuilder";
+import _AttachmentOptionBuilder from "./ApplicationCommand/options/AttachmenteOptionBuilder";
+import _BooleanOptionBuilder from "./ApplicationCommand/options/BooleanOptionBuilder";
+import _ChannelOptionBuilder from "./ApplicationCommand/options/ChannelOptionBuilder";
+import _IntegerOptionBuilder from "./ApplicationCommand/options/IntegerOptionBuilder";
+import _MentionableOptionBuilder from "./ApplicationCommand/options/MentionableOptionBuilder";
+import _NumberOptionBuilder from "./ApplicationCommand/options/NumberOptionBuilder";
+import _RoleOptionBuilder from "./ApplicationCommand/options/RoleOptionBuilder";
+import _StringOptionBuilder from "./ApplicationCommand/options/StringOptionBuilder";
+import _SubCommandGroupOptionBuilder from "./ApplicationCommand/options/SubCommandGroupOptionBuilder";
+import _SubCommandOptionBuilder from "./ApplicationCommand/options/SubCommandOptionBuilder";
+import _UserOptionBuilder from "./ApplicationCommand/options/UserOptionBuilder";
+import _UserCommandBuilder from "./ApplicationCommand/UserCommandBuilder";
+import _CanvasUtil from "./Canvas";
+import _EmbedBuilder from "./Message/EmbedBuilder";
+import _EmbedColors from "./Message/EmbedColors";
+import _getEmoji from "./Utilities/getEmoji";
+import _GifAPI from "./Utilities/GifAPI";
 
-const data = {
-    getEmoji, GifAPI,
-    EmbedBuilder,
-    EmbedColors,
-    CanvasUtil,
-    ChatInputCommandBuilder,
-    MessageCommandBuilder,
-    UserCommandBuilder,
-    AttachmentOptionBuilder,
-    BooleanOptionBuilder,
-    ChannelOptionBuilder,
-    IntegerOptionBuilder,
-    MentionableOptionBuilder,
-    NumberOptionBuilder,
-    RoleOptionBuilder,
-    StringOptionBuilder,
-    SubCommandGroupOptionBuilder,
-    SubCommandOptionBuilder,
-    UserOptionBuilder,
-};
+export const getEmoji = _getEmoji;
 
-export default data;
+export const GifAPI = _GifAPI;
+
+export const EmbedBuilder = _EmbedBuilder;
+
+export const EmbedColors = _EmbedColors;
+
+export const CanvasUtil = _CanvasUtil;
+
+export const ChatInputCommandBuilder = _ChatInputCommandBuilder;
+
+export const MessageCommandBuilder = _MessageCommandBuilder;
+
+export const UserCommandBuilder = _UserCommandBuilder;
+
+export const AttachmenteOptionBuilder = _AttachmentOptionBuilder;
+
+export const BooleanOptionBuilder = _BooleanOptionBuilder;
+
+export const ChannelOptionBuilder = _ChannelOptionBuilder;
+
+export const IntegerOptionBuilder = _IntegerOptionBuilder;
+
+export const MentionableOptionBuilder = _MentionableOptionBuilder;
+
+export const NumberOptionBuilder = _NumberOptionBuilder;
+
+export const RoleOptionBuilder = _RoleOptionBuilder;
+
+export const StringOptionBuilder = _StringOptionBuilder;
+
+export const SubCommandGroupOptionBuilder = _SubCommandGroupOptionBuilder;
+
+export const SubCommandOptionBuilder = _SubCommandOptionBuilder;
+
+export const UserOptionBuilder = _UserOptionBuilder;
